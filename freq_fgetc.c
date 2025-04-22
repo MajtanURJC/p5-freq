7fdddedb3462a31c53fc88e3cf17e635feff399a
”majtan” <ma.novo.2023@alumnos.urjc.es>
2025-04-22 19:06:09 +0200

    Añadir practica y ejemplos

--- freq_fgetc.c
+++ freq_fgetc.c
@@ -5,54 +5,47 @@
 #include <string.h>
 
 enum {
-    max_size = 128,
-    buffer_size = 4028,
+    max_size = 128, 
 };
 
-void
-printArray(int *array, int num) 
-{
-
-    for (int i = 0; i < max_size; i++) {
+void 
+printArray(int *array, int num) {
+    int i;
+    for (i = 0; i < max_size; i++) {
         if (array[i] != 0) {
+
             printf("%#04x: %f\n", i, (double) array[i] / num);
         }
     }
 }
 
-int
-main(int argc, char *argv[]) 
-{
+int 
+main(int argc, char *argv[]) {
     argv++;
     argc--;
+    int i;
     int ascci[max_size];
-    int leidos;
-    int j;
-    int posicion;
+    int caracter;
     FILE *archivo;
     int caracteres = 0;
-    unsigned char buffer[buffer_size];
 
     // Inicializamos el arreglo ascci con ceros
     memset(ascci, 0, max_size * sizeof(int));
 
-    for (int i = 0; i < argc; i++) {
-        archivo = fopen(argv[i], "r"); 
+    for (i = 0; i < argc; i++) {
+        archivo = fopen(argv[i], "r");
 
         if (archivo == NULL) {
             perror("Error al abrir el archivo");
             return 1;
         }
-        while ((leidos = fread(buffer, sizeof(unsigned char), buffer_size, archivo)) > 0) {
-            for (j = 0; j < leidos; j++) {
-                if (buffer[j] < max_size) {
-                    posicion = (int) buffer[j];
-                    ascci[posicion]++;
-                    caracteres++;
-                }
+
+        while ((caracter = fgetc(archivo)) != EOF) {
+            if (caracter >= 0 && caracter < max_size) {
+                ascci[caracter]++;
+                caracteres++;
             }
         }
-
         fclose(archivo);
     }
 
@@ -63,4 +56,5 @@ main(int argc, char *argv[])
     }
 
     return 0;
+    
 }
